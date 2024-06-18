9aa73dd76201e79f4d5f1bd2ff19e673f36e9145
stivka <kaptenstiv@gmail.com>
2024-06-18 16:44:17 +0300

    change allowed origin to stivka.net

--- src/main/java/ee/stivka/luka/config/WebConfig.java
+++ src/main/java/ee/stivka/luka/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://luka-api-production.up.railway.app/")
+                .allowedOrigins("http://localhost:3000", "https://stivka.net/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
