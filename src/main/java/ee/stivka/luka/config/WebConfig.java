a49778205f17bc1a67a382236b102e5fb3b9773b
stivka <kaptenstiv@gmail.com>
2024-06-26 12:42:15 +0300

    Add new domain name to cors whitelist

--- src/main/java/ee/stivka/luka/config/WebConfig.java
+++ src/main/java/ee/stivka/luka/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://stivka.net/")
+                .allowedOrigins("http://localhost:3000", "https://haveyouseenluka.com", "https://www.haveyouseenluka.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
