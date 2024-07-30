5d8da63c5e5645b749a587a0efcdffcea4797d4b
Markus Tarn <markustarn@gmail.com>
2024-07-30 14:55:14 +0300

    Trying new timezone

--- src/main/java/ee/stivka/luka/LukaApplication.java
+++ src/main/java/ee/stivka/luka/LukaApplication.java
@@ -1,5 +1,7 @@
 package ee.stivka.luka;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class LukaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Timezone: " + System.getProperty("user.timezone"));
+		TimeZone timeZone = TimeZone.getDefault();
+		System.out.println("Current Timezone: " + timeZone.getID());
 		SpringApplication.run(LukaApplication.class, args);
 	}
 }
