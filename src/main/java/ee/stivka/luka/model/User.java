27f8c4d66bc8622fd9a3758e489eec7940402cd5
Stiv Kapten <kaptenstiv@gmail.com>
2024-07-30 14:07:07 +0300

    Revert "Revert "Added print timezone on service start for testing""

--- src/main/java/ee/stivka/luka/model/User.java
+++ src/main/java/ee/stivka/luka/model/User.java
@@ -2,9 +2,7 @@ package ee.stivka.luka.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
