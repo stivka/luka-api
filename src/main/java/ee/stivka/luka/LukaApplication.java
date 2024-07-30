8a1ac9ca45525b4643a6569b2e03c315ee8245f7
Markus Tarn <markustarn@gmail.com>
2024-07-30 17:56:03 +0300

    Potential fix to timezone problem

--- src/main/java/ee/stivka/luka/LukaApplication.java
+++ src/main/java/ee/stivka/luka/LukaApplication.java
@@ -1,7 +1,5 @@
 package ee.stivka.luka;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,9 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class LukaApplication {
 
-	public static void main(String[] args) {
-		TimeZone timeZone = TimeZone.getDefault();
-		System.out.println("Current Timezone: " + timeZone.getID());
-		SpringApplication.run(LukaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LukaApplication.class, args);
+    }
 }
